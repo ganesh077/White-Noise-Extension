b64358da0f5ae78dff99cdb6340bb0fb20cd8d8d
ganesh077 <ganeshthampi077@gmail.com>
2023-12-25 19:44:37 -0500

    infinite loop

--- background.js
+++ background.js
@@ -4,7 +4,19 @@ var audio = new Audio('white_noise.mp3');
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.local.set({ playing: false });
+  addAudioEventListeners();
 });
+
+function addAudioEventListeners() {
+  audio.addEventListener('ended', function () {
+    chrome.storage.local.get('playing', function (data) {
+      if (data.playing) {
+        audio.currentTime = 0; // Reset the playback position
+        audio.play();
+      }
+    });
+  });
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'togglePlay') {
@@ -18,3 +30,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     });
   }
 });
+
+// Add this line to restart audio playback if the extension is reloaded
+addAudioEventListeners();
+
